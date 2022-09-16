77a8312eaf4fbc4962cc424be1f100c586e5b68f
leoarthurwall <leowall93@gmail.com>
2022-09-16 16:09:00 +0100

    added css for form

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      <div className='form-container'>
+        <input 
+          className='form-field'
+          placeholder='First Name'
+          name='firstName'
+        />
+        <input
+          className='form-field'
+          placeholder='Last Name'
+          name='lastName'
+        />
+        <input
+          className='form-field'
+          placeholder='Email'
+          name='Email'
+        />
+        <button className='register-button'>Register</button>
+      </div>
     </div>
   );
 }

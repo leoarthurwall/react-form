015382a78d082086939cd222c1201440de637626
leoarthurwall <leowall93@gmail.com>
2022-09-16 16:53:52 +0100

    added on change handlers for all of the input fields, so now you can write in each and if you console loge the event-target.value, you'll see the respective text being inputtet

--- src/App.js
+++ src/App.js
@@ -9,22 +9,37 @@ function App() {
     email: "",
   })
 
+  const handleChangeFirstNameInput = (event) => {
+    //update values state, take copy of values(spread operator) object, change firstname of copy
+    setValues({...values, firstName: event.target.value})
+  }
+  const handleChangeLastNameInput = (event) => {
+    setValues({...values, lastName: event.target.value})
+  }
+  const handleChangeEmailInput = (event) => {
+    setValues({...values, email: event.target.value})
+  }
+
   return (
     <div className="app">
       <div className='form-container'>
+        <div className='success-message'>Success! Thanks for registering</div>
         <input 
+          onChange={handleChangeFirstNameInput}
           value={values.firstName}
           className='form-field'
           placeholder='First Name'
           name='firstName'
         />
         <input
+        onChange={handleChangeLastNameInput}
           value={values.lastName}
           className='form-field'
           placeholder='Last Name'
           name='lastName'
         />
         <input
+          onChange={handleChangeEmailInput}
           value={values.email}
           className='form-field'
           placeholder='Email'

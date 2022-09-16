6c8a0674396f596a4eb11c725e4ff53c28f0c720
leoarthurwall <leowall93@gmail.com>
2022-09-16 16:15:22 +0100

    imported usestate from react, create a values useState and assigned each imput box a value

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
+import { useState } from 'react';
 import './index.css';
 
 function App() {
-  
+
+  const[values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  })
 
   return (
     <div className="app">
       <div className='form-container'>
         <input 
+          value={values.firstName}
           className='form-field'
           placeholder='First Name'
           name='firstName'
         />
         <input
+          value={values.lastName}
           className='form-field'
           placeholder='Last Name'
           name='lastName'
         />
         <input
+          value={values.email}
           className='form-field'
           placeholder='Email'
           name='Email'

18c4a30727b33ab6d979fa89549d83890471fcdf
leoarthurwall <leowall93@gmail.com>
2022-09-16 17:03:31 +0100

    added an on submit handler for the success message. currently not functional

--- src/App.js
+++ src/App.js
@@ -1,51 +1,60 @@
-import { useState } from 'react';
-import './index.css';
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-
-  const[values, setValues] = useState({
+  const [values, setValues] = useState({
     firstName: "",
     lastName: "",
     email: "",
-  })
+  });
+
+  const [submitted, setSubmitted] = useState(false);
 
   const handleChangeFirstNameInput = (event) => {
     //update values state, take copy of values(spread operator) object, change firstname of copy
-    setValues({...values, firstName: event.target.value})
-  }
+    setValues({ ...values, firstName: event.target.value });
+  };
   const handleChangeLastNameInput = (event) => {
-    setValues({...values, lastName: event.target.value})
-  }
+    setValues({ ...values, lastName: event.target.value });
+  };
   const handleChangeEmailInput = (event) => {
-    setValues({...values, email: event.target.value})
+    setValues({ ...values, email: event.target.value });
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitted(true);
   }
 
   return (
     <div className="app">
-      <div className='form-container'>
-        <div className='success-message'>Success! Thanks for registering</div>
-        <input 
+      <div className="form-container" onSubmit={handleSubmit}>
+        {submitted ? (
+          <div className="success-message">Success! Thanks for registering</div>
+        ) : null}
+        <input
           onChange={handleChangeFirstNameInput}
           value={values.firstName}
-          className='form-field'
-          placeholder='First Name'
-          name='firstName'
+          className="form-field"
+          placeholder="First Name"
+          name="firstName"
         />
         <input
-        onChange={handleChangeLastNameInput}
+          onChange={handleChangeLastNameInput}
           value={values.lastName}
-          className='form-field'
-          placeholder='Last Name'
-          name='lastName'
+          className="form-field"
+          placeholder="Last Name"
+          name="lastName"
         />
         <input
           onChange={handleChangeEmailInput}
           value={values.email}
-          className='form-field'
-          placeholder='Email'
-          name='Email'
+          className="form-field"
+          placeholder="Email"
+          name="Email"
         />
-        <button className='register-button'>Register</button>
+        <button className="register-button">Register</button>
       </div>
     </div>
   );
